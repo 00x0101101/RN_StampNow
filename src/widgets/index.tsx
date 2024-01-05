867336437350d6c29dc034e2e5c47a204a027260
00x0101101 <36686082+00x0101101@users.noreply.github.com>
2024-01-05 15:54:38 +0800

    Feature Polish Update
    1. Change `.setText` to `.insertRichText`, avoiding unexpected caret shifts.
    
    2.  reference to stamps won't be created when focused rem is blank and is one child of today's daily document, or it would seem duplicate in the daily doc.

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -10,12 +10,11 @@ import '../style.css';
 import '../App.css';
 import moment from 'moment';
 
-
-// origin definition is in "consts.tsx" under the project:
-// [GTD_On_RN](https://github.com/00x0101101/GTD_On_RN)
+//region const definitions
+// external const value:origin definition is in "consts.tsx" under the project: [GTD_On_RN](https://github.com/00x0101101/GTD_On_RN)
 const TICK_PW = 'time_tick';
 const TICK_SLOT = 'tkty';
-
+//endregion
 
 async function onActivate(plugin: ReactRNPlugin) {
 	const getAllPropOf = async (tagRem: Rem | undefined) => {
@@ -30,13 +29,12 @@ async function onActivate(plugin: ReactRNPlugin) {
 		action: async () => {
 			let stamp = await createStamp(new Date());
 			let thisRem = await plugin.focus.getFocusedRem();
-			if (stamp && thisRem) {
+			if (stamp && thisRem && stamp._id!==thisRem._id) {
+
 				// await stamp.copyReferenceToClipboard();
 				// await plugin.app.toast('Stamp Copied!');
 				let stampElement = (await plugin.richText.rem(stamp).value());
-				let thisText = thisRem.text?.concat(stampElement);
-				if (thisText)
-					thisRem?.setText(thisText);
+				await plugin.editor.insertRichText(stampElement);
 			}
 		},
 
@@ -51,18 +49,29 @@ async function onActivate(plugin: ReactRNPlugin) {
 		let stampRichText = await plugin.richText.text(stampText).value();
 
 		let daily = await plugin.date.getDailyDoc(date);
+		let focus=await plugin.focus.getFocusedRem();
 		if (!(daily)) {
 			await plugin.app.toast('Failed to Locate Dairy');
 			return;
 		}
-		let stamp = (await plugin.rem.findByName(stampRichText, daily._id)) || (await plugin.rem.createRem());
+		let stamp:Rem|undefined;
+		//whether a reference to stamp is not to create
+		let refNeedless=false;
+		//if focused rem is blank and one child of today's dailyDoc, the reference need not create.
+		if(focus&&focus.parent===daily._id&&!focus.text?.length)
+		{
+			stamp=focus;
+			refNeedless=true;
+		}
+		else
+			stamp = (await plugin.rem.findByName(stampRichText, daily._id)) || (await plugin.rem.createRem());
 		stamp?.setType(SetRemType.DESCRIPTOR);
 		if (!stamp) {
 			await plugin.app.toast('Failed to Create Stamp');
 			return;
 		}
 
-		await stamp.setParent(daily);
+		if(!refNeedless)await stamp.setParent(daily);
 		await stamp.setText(stampRichText);
 
 		//add corresponding tags for the stamp if the plugin "GTD_On_RN" (aka. "GTD workflow enhancement") has been installed
